7ddce985d64782624b49cc924b161fc6ac3f9eac
Tri Ardini <tr.ardn@gmail.com>
2020-01-19 21:07:18 +0700

    add sample query schema

--- internal/app/graphql/query/schema.go
+++ internal/app/graphql/query/schema.go
@@ -1,10 +1,25 @@
 package query
 
-import "github.com/graphql-go/graphql"
+import (
+	"github.com/graphql-go/graphql"
+	"github.com/kitabisa/buroq/internal/app/graphql/types"
+)
 
 // GetQuerySchema schema for query / get record
 func GetQuerySchema() graphql.ObjectConfig {
-	queryFields := graphql.Fields{}
+	queryFields := graphql.Fields{
+		"books": &graphql.Field{
+			Type: types.BookType,
+		},
+		"book": &graphql.Field{
+			Type: graphql.NewList(types.BookType),
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.Int),
+				},
+			},
+		},
+	}
 
 	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: queryFields}
 
